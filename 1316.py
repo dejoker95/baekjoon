ec129a03d7f0a930521b1cbba43b2f8ad914870d
dejoker95 <dejoker95@gmail.com>
2021-04-01 01:36:29 +0900

    [feat] format change

--- 1316.py
+++ 1316.py
@@ -1,14 +1,16 @@
 n = int(input())
 
+
 def check(word):
     c = set()
-    prev = ''
+    prev = ""
     for char in word:
         if (char in c) and (prev != char):
             return False
         c.add(char)
         prev = char
     return True
+
 
 count = 0
 for i in range(n):
