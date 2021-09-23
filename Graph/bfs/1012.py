b5dcd30d5aa703d9eced9d8dd4d3cc9afb3d31d1
dejoker95 <dejoker95@gmail.com>
2021-09-24 01:01:39 +0900

    2021-09-23

--- Graph/bfs/1012.py
+++ Graph/bfs/1012.py
@@ -1,5 +1,43 @@
 import sys
+from collections import deque
 
-read = sys.stdin.readline
-T = int(read())
-M, N, K = map(int, read().split())
+
+def bfs(graph, visited, start):
+    i, j = start
+    queue = deque()
+    queue.append((i, j))
+    visited[i][j] = 1
+    nx = [-1, 1, 0, 0]
+    ny = [0, 0, -1, 1]
+    while queue:
+        i, j = queue.popleft()
+
+        for x, y in zip(nx, ny):
+            ni = i + x
+            nj = j + y
+            if 0 <= ni < N and 0 <= nj < M:
+                if graph[ni][nj] == 1 and visited[ni][nj] == 0:
+                    queue.append((ni, nj))
+                    visited[ni][nj] = 1
+
+
+if __name__ == "__main__":
+    read = sys.stdin.readline
+    T = int(read())
+
+    for _ in range(T):
+        M, N, K = map(int, read().split())
+        visited = [[0] * M for _ in range(N)]
+        graph = [[0] * M for _ in range(N)]
+
+        for _ in range(K):
+            x, y = map(int, read().split())
+            graph[y][x] = 1
+
+        cnt = 0
+        for i in range(N):
+            for j in range(M):
+                if graph[i][j] == 1 and visited[i][j] == 0:
+                    bfs(graph, visited, (i, j))
+                    cnt += 1
+        print(cnt)
--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-import sys
+x = [-1, 1, 0, 0]
+y = [0, 0, -1, 1]
 
-N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().split()))
-ans = [-1] * N
-stack = [0]
-
-for i in range(1, N):
-    while stack and A[stack[-1]] < A[i]:
-        ans[stack[-1]] = A[i]
-        stack.pop()
-    stack.append(i)
-
-print(*ans)
+for i, j in zip(x, y):
+    print(i, j)
